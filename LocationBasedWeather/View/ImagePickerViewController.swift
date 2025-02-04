a426484f5899498cd76260c810086290fbddd7fa
Park Junwoo <ays99111@naver.com>
2025-02-04 17:23:25 +0900

    [feat] ImagePickerVC 생성

---
+++ LocationBasedWeather/View/ImagePickerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ImagePickerViewController.swift
+//  LocationBasedWeather
+//
+//  Created by 박준우 on 2/4/25.
+//
+
+import UIKit
+
+class ImagePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.cyan
+    }
+}
