3c26caec6f74acbf439a562b052d8147a073dab3
Park Junwoo <ays99111@naver.com>
2025-02-04 17:13:55 +0900

    [chore] 폴더링

---
+++ LocationBasedWeather/Network/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  LocationBasedWeather
+//
+//  Created by 박준우 on 2/4/25.
+//
+
+import Foundation
+
+enum APIKey {
+    static let openWeather: String = "8b47676c673d0c5f1a84b18c523ea99e"
+}
