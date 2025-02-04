5ad56f4f4f1fb51ad45d25608af32cc7af3ddc89
Park Junwoo <ays99111@naver.com>
2025-02-04 17:43:59 +0900

    [feat] ImagePickerVC 뷰 구현

--- LocationBasedWeather/View/ImagePickerViewController.swift
+++ LocationBasedWeather/View/ImagePickerViewController.swift
@@ -7,10 +7,66 @@
 
 import UIKit
 
-class ImagePickerViewController: UIViewController {
+import SnapKit
 
+final class ImagePickerViewController: UIViewController {
+    
+    private lazy var imagePickerButton: UIButton = {
+        let btn: UIButton = UIButton()
+        var configure = UIButton.Configuration.filled()
+        configure.title = "ImagePickerController"
+        configure.baseBackgroundColor = UIColor.black
+        configure.baseForegroundColor = UIColor.orange
+        configure.cornerStyle = .capsule
+        btn.configuration = configure
+        btn.addTarget(self, action: #selector(self.imagePickerTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var phPickerButton: UIButton = {
+        let btn: UIButton = UIButton()
+        var configure = UIButton.Configuration.filled()
+        configure.title = "PHPickerViewController"
+        configure.baseBackgroundColor = UIColor.gray
+        configure.baseForegroundColor = UIColor.black
+        configure.cornerStyle = .capsule
+        btn.configuration = configure
+        btn.addTarget(self, action: #selector(self.phPickerTapped), for: .touchUpInside)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.cyan
+        self.view.backgroundColor = UIColor.white
+        
+        self.configureHierarchy()
+        self.configureLayout()
+    }
+    
+    private func configureHierarchy() {
+        self.view.addSubview(self.imagePickerButton)
+        self.view.addSubview(self.phPickerButton)
+    }
+    
+    private func configureLayout() {
+        self.imagePickerButton.snp.makeConstraints { make in
+            make.width.equalTo(250)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-50)
+        }
+        
+        self.phPickerButton.snp.makeConstraints { make in
+            make.width.equalTo(250)
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(50)
+        }
+    }
+    
+    @objc private func imagePickerTapped() {
+        print(#function)
+    }
+    
+    @objc private func phPickerTapped() {
+        print(#function)
     }
 }
